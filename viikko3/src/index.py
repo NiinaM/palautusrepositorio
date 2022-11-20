007aa4121b2442676434b987d14605d9f5ffb7a9
Niina-Maria Takalo <niina-maria.takalo@helsinki.fi>
2022-11-20 23:46:31 +0200

    Viikko 3 tehtävä 2

--- viikko3/src/index.py
+++ viikko3/src/index.py
@@ -15,13 +15,18 @@ def main():
             player_dict['name'],
             player_dict['team'],
             player_dict['goals'],
-            player_dict['assists']
+            player_dict['assists'],
+            player_dict['nationality']
             
         )
 
         players.append(player)
 
+    finnish_players = sorted(filter(lambda x: x.nationality == "FIN", players), key=lambda x: x.count_goals_assists(), reverse=True)
+
     print("Oliot:")
 
-    for player in players:
+    for player in finnish_players:
         print(player)
+
+main()
--- viikko3/src/player.py
+++ viikko3/src/player.py
@@ -1,9 +1,16 @@
 class Player:
-    def __init__(self, name, team, goals, assists):
+    def __init__(self, name, team, goals, assists, nationality):
         self.name = name
         self.team = team
         self.goals = goals
         self.assists = assists
+        self.nationality = nationality
+
     
+    def count_goals_assists(self):
+        sum_goals_assists = self.goals + self.assists
+        return sum_goals_assists
+        
+
     def __str__(self):
-        return self.name + " team: " + str(self.team) + " goals: " + str(self.goals) + " assists: " + str(self.assists)
+        return f"{self.name:20}" + " team: " + str(self.team) + " " + str(self.goals) + " + " + str(self.assists) + " = " + str(self.count_goals_assists())
